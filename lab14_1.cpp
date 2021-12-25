862f571570ce76b44ccfe531d40f11722144237c
nunu111 <nunu111za@hotmail.com>
2021-12-25 11:59:53 +0700

    all done

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,45 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double hide[],int A_size,double show[]){
+
+    double X=0,Y=1,Z=0,deviation=0;
+
+    //หาค่าเฉลี่ยเลขคณิต
+    for (int i=0;i<A_size;i++){
+        X += hide[i];
+    }
+    show[0]= X/A_size;
+
+    //หาส่วนเบี่ยงเบนมาตรฐาน
+    for(int i=0;i<A_size;i++){
+        deviation += pow(hide[i]-show[0],2);
+    }
+    deviation = deviation/A_size;
+    deviation = sqrt(deviation);
+    show[1] = deviation;
+
+    //หาค่าเฉลี่ยเรขาคณิต
+    for(int i=0;i<A_size;i++){
+        Y *= hide[i];
+    }
+    show[2] = pow(Y,(1.0/A_size));
+
+    //หาค่าเฉลี่ยฮาร์โมนิก
+    for(int i=0;i<A_size;i++){
+        Z += (1.0/hide[i]);
+    }
+    show[3] =  A_size/Z;
+
+    //หาค่าสูงสุดและค่าต่ำสุด
+    for(int i=0;i<A_size;i++){
+        if (i==0) {
+            show[4]=hide[i];
+            show[5]=hide[i];
+        }
+        else if (hide[i]>show[4]) show[4]=hide[i];
+        else if (hide[i]<show[5]) show[5]=hide[i];
+       
+    }
+}
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,32 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool brush[][M],int size,int pixel_x,int pixel_y){
+    for (int i=0;i<N;i++){
+        for (int j=0;j<M;j++){
+            if (pow((pow(i-pixel_x,2)+pow((j-pixel_y),2)),1.0/2)<=size-1){
+            brush [i][j]=1;
+            }
+        }
+    }
+
+}
+
+void showImage(const bool paper[][M]){
+    cout << "------------------------------------------------------------------------"<<endl;
+    for(int i=0;i<N;i++){
+        cout << "|";
+        for(int j=0;j<M;j++){
+            if(paper[i][j]){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            } 
+        }
+        cout << "|" <<endl;
+    }
+    cout << "------------------------------------------------------------------------"<<endl;
+
+}
